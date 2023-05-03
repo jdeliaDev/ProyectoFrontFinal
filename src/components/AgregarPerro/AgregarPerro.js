9777fac350411e8028fccdb199242db25f50941e
jdeliaDev <jdelia@bld.com.ar>
2023-05-03 08:29:35 -0300

    Actualizando

--- src/components/AgregarPerro/AgregarPerro.js
+++ src/components/AgregarPerro/AgregarPerro.js
@@ -7,33 +7,33 @@ export default function AgregarPerro() {
         <Fragment>
             <Navegacion/>
             <main>
-            <form class="container d-flex flex-column justify-content-center formulario" action="" method="POST">
-                <div class="row">
-                    <label for="nombre" class="col-sm-2 col-form-label etiquetas">Nombre</label>
-                    <div class="col-sm-8">
-                        <input type="text" class="form-control" placeholder="Nombre" id="nombre" name="nombre" required/>
+            <form className="container d-flex flex-column justify-content-center formulario" action="" method="POST">
+                <div className="row">
+                    <label htmlFor="nombre" className="col-sm-2 col-form-label etiquetas">Nombre</label>
+                    <div className="col-sm-8">
+                        <input type="text" className="form-control" placeholder="Nombre" id="nombre" name="nombre" required/>
                     </div>
                 </div>
-                <div class="row">
-                    <label for="padres" class="col-sm-2 col-form-label etiquetas">Padres</label>
-                    <div class="col-sm-8">
-                        <input type="text" class="form-control" placeholder="Padres" id="padres" name="padres" required/>
+                <div className="row">
+                    <label htmlFor="padres" className="col-sm-2 col-form-label etiquetas">Padres</label>
+                    <div className="col-sm-8">
+                        <input type="text" className="form-control" placeholder="Padres" id="padres" name="padres" required/>
                     </div>
                 </div>
-                <div class="row">
-                    <label for="fechaNacimiento" class="col-sm-2 col-form-label etiquetas">Fecha Nacimiento</label>
-                    <div class="col-sm-8">
-                        <input type="date" class="form-control" placeholder="Fecha Nacimiento" id="fechaNacimiento" name="fechaNacimiento" required/>
+                <div className="row">
+                    <label htmlFor="fechaNacimiento" className="col-sm-2 col-form-label etiquetas">Fecha Nacimiento</label>
+                    <div className="col-sm-8">
+                        <input type="date" className="form-control" placeholder="Fecha Nacimiento" id="fechaNacimiento" name="fechaNacimiento" required/>
                     </div>
                 </div>
-                <div class="row">
-                    <label for="imagen" class="col-sm-2 col-form-label etiquetas">Imágen</label>
-                    <div class="col-sm-8">
-                        <input type="file" class="form-control" id="imagen" accept="image/*" onChange="" required/>
+                <div className="row">
+                    <label htmlFor="imagen" className="col-sm-2 col-form-label etiquetas">Imágen</label>
+                    <div className="col-sm-8">
+                        <input type="file" className="form-control" id="imagen" accept="image/*" onChange="" required/>
                     </div>
                 </div>
-                <div class="row">
-                    <input type="submit" value="Agregar" class="btn botones col-sm-2"/>
+                <div className="row">
+                    <input type="submit" value="Agregar" className="btn botones col-sm-2"/>
                 </div>
                 </form>
 
--- src/components/Tarjeta/Tarjeta.js
+++ src/components/Tarjeta/Tarjeta.js
@@ -5,7 +5,7 @@ export default function Tarjeta ({info}) {
     return(
         <div className="card m-2 p-2 d-flex flex-row justify-content-between align-items-center">
             <div className='d-flex flex-column justify-content-between align-items-center'>
-                <img className='imgTamaño' src={info.imagen}/>
+                <img className='imgTamaño' src={info.imagen} alt='perro'/>
                 <p>{info.nombre}</p>
                 <p>Padres: {info.padres}</p>
                 <p>Fecha Nacimiento: {info.fechaNacimiento}</p>
