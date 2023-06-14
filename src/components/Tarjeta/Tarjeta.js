7dab2b383d272bc4cee646e558348b22101cf696
jdeliaDev <jdelia@bld.com.ar>
2023-06-14 09:15:37 -0300

    actualizar

--- src/components/Tarjeta/Tarjeta.js
+++ src/components/Tarjeta/Tarjeta.js
@@ -27,29 +27,25 @@ export default function Tarjeta ({info}) {
     return(
         <Fragment> 
             { eliminado === false? 
-            <div className="card m-2 p-2 d-flex flex-row justify-content-between align-items-center">
-                <div className='d-flex flex-column justify-content-between align-items-center'>
-                    <img className='imgTamaño' src={info.imagen} alt='perro'/>
-                    <p>{info.nombre}</p>
-                    <p>Padres: {info.padres}</p>
-                    <p>Fecha Nacimiento: {info.fechaNacimiento}</p>
-                    {info.sexo === 'M'? 
-                        <div>
-                            <Link className="btn botones m-3 p-2" to="/ModificarPerroMacho" onClick={()=>dataPerro()}>Editar</Link> 
-                            <button className="btn botones m-3 p-2" onClick={()=>eliminarPerro()}>Eliminar</button>  
-                        </div>
-                        :
-                        <div>
-                            <Link className="btn botones m-3 p-2" to="/ModificarPerroHembra" onClick={()=>dataPerro()}>Editar</Link> 
-                            <button className="btn botones m-3 p-2" onClick={()=>eliminarPerro()}>Eliminar</button>  
-                        </div>
-                    }
-                </div>
+                <div className="card m-2 p-2 d-flex flex-row justify-content-between align-items-center">
+                    <div className='d-flex flex-column justify-content-between align-items-center'>
+                        <img className='imgTamaño' src={info.imagen} alt='perro'/>
+                        <p>{info.nombre}</p>
+                        <p>Padres: {info.padres}</p>
+                        <p>Fecha Nacimiento: {info.fechaNacimiento}</p>
+                        {info.sexo === 'M'? 
+                            <div>
+                                <Link className="btn botones m-3 p-2" to="/ModificarPerroMacho" onClick={()=>dataPerro()}>Editar</Link> 
+                                <button className="btn botones m-3 p-2" onClick={()=>eliminarPerro()}>Eliminar</button>  
+                            </div>
+                            :
+                            <div>
+                                <Link className="btn botones m-3 p-2" to="/ModificarPerroHembra" onClick={()=>dataPerro()}>Editar</Link> 
+                                <button className="btn botones m-3 p-2" onClick={()=>eliminarPerro()}>Eliminar</button>  
+                            </div>
+                        }
+                    </div>
             </div>:""     
-            //<div className="alert alert-dismissible alert-success">
-                    //<strong> Perro eliminado Exitosamente!</strong>
-                    //<Link to="/" >Aceptar</Link>
-                //</div> ""
             }
         </Fragment>
     )
